cefdb4a66f7020c21e0444d01ae180267dc04182
nishansaini <nishan.saini@gmail.com>
2021-04-26 23:56:17 -0400

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "${var.resource_group}"
-    storage_account_name = "tstate6805"
+    storage_account_name = "tstate680533"
     container_name       = "tstate"
     key                  = "terraform.tfstate"
   }
