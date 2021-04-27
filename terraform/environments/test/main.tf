af547f5277728440a81d59169c70244f0bec6041
Singh, Nishan (ns142e) <ns142e@att.com>
2021-04-26 23:49:04 -0400

    initial

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -8,7 +8,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "${var.resource_group}"
-    storage_account_name = "tstate23125"
+    storage_account_name = "tstate6805"
     container_name       = "tstate"
     key                  = "terraform.tfstate"
   }
